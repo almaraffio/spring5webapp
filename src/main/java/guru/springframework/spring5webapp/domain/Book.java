f0fa1b43cc874b4356b984f39918310d8db9be1b
almaraffio <aiden.l.maraffio@gmail.com>
2025-10-07 20:01:43 -0600

    Initializing Data

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -21,15 +22,15 @@ public class Book {
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book(){
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn/*, Set<Author> authors*/) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
+        //this.authors = authors;
     }
 
     public Long getId() {
